45e3e4ea8ceb809cf13edc1bba03d1b6c551f250
sandro_herbst <sandro.herbst@gmail.com>
2019-11-23 12:45:17 -0300

    Module 4

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,12 +10,11 @@ class Quiz extends Component {
         this.state = { quiz_position: 1}
     }
     render() {
-        const isQuizEnd = ((this.state.quiz_position -1) === quizData.quiz_questions.lenght)
+        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
         return (
-            <div>
-                {isQuizEnd ? <QuizEnd /> : 
-                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
-            </div>
+          <div>
+            {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
+          </div>
         )
     }
 }
