8bea7a61fa03002a115995dbd6fecb9291c7c86a
sandro_herbst <sandro.herbst@gmail.com>
2019-11-23 13:53:54 -0300

    End

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,7 +15,7 @@ class Quiz extends Component {
         })
     }
     handleResetClick() {
-        this.props.resetClickHandler()
+        this.setState({quiz_position: 1})
     }
     render() {
         const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
